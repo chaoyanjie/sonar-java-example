62d8ee407997c9d068729e5b5606c1578d6ca314
w13161876142 <408768085@qq.com>
2016-05-10 11:31:22 +0800

    edit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
+    public String s = "new world";
 
     public static void main(String[] args) {
         System.out.println("Hello World !");
+        System.out.println(s);
     }
 }
